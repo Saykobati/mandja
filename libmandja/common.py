fa7d770247861da1552cb6525de60397d9798ac4
mr6r4y <mr6r4y@741b1206-e63e-97db-7ddf-8b48874080b5>
2010-12-16 21:41:15 +0000

    Finish with basic Proxy class and make Proxy.getHostByName working. This method can be used to resolve domain name through tor proxy.

--- libmandja/common.py
+++ libmandja/common.py
@@ -1,17 +1,21 @@
 import re
 
-def isint( value ):
+def isint(value):
     """ Check if 'value' is integer """
     
-    isint = True
+    result = True
     try:
-        num = int(str)
-    except ValueError:
-        isint = False
-    return isint
+        num = int(value)
+    except (ValueError, TypeError):
+        result = False
+        
+    return result
 
 
 def isipaddr(ip_str):
+    if not isinstance(ip_str, str):
+        return False
+    
     pattern = r"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"\
               r"\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"\
               r"\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"\
