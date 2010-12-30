4fdaf94a8f0c00bd0af75f7f8331b7e827a7ab95
mr6r4y <mr6r4y@741b1206-e63e-97db-7ddf-8b48874080b5>
2010-12-30 15:50:02 +0000

    There is a main crawling loop which can traverse trough some URL. For now the functionality is basic. In future I will make it more flexible and I will add various extentions.

--- mandja.py
+++ mandja.py
@@ -1,71 +1,22 @@
 #!/usr/bin/env python
 
-import httplib2
-import urllib2
-from restkit import request
-import urlparse
-
-import os
-import sys
-
 from libmandja.cmdline import cmdlineParse
-from libmandja.parser import getURLs
-
-__default_headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',\
-                     'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; '\
-                                   'Windows NT 6.0; SU 3.1; SLCC1; .NET '\
-                                   'CLR 2.0.50727; .NET CLR 3.0.04506; '\
-                                   '.NET CLR 1.1.4322; .NET CLR 3.5.21022)',\
-                     'Accept-Language': 'en-us,en;q=0.5',\
-                     'Accept-Encoding': 'gzip,deflate',\
-                     'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7'}
-
-def headPing(url):
-    if(not isinstance(url, urlparse.ParseResult)):
-        url = urlparse.urlparse(url)
-    
-    conn = httplib2.Http()
-    conn.follow_redirects = False
-    (headers, body) = conn.request(url.geturl(), 'HEAD', None, None)
-    
-    return headers
-
-def getContent(url):
-    if(not isinstance(url, urlparse.ParseResult)):
-        url = urlparse.urlparse(url)
-    
-    req = urllib2.Request(url.geturl(), None, __default_headers)
-    resp = urllib2.urlopen(req)
-    
-    return resp
-
-def getContent2(url):
-    if(not isinstance(url, urlparse.ParseResult)):
-        url = urlparse.urlparse(url)
-    
-    response = request(url.geturl(), headers = __default_headers)
-    
-    return response
-
+from libmandja.crawler import Crawl
 
 def main():
     arguments = cmdlineParse()
     main_url = arguments.url
     
+    cr_ob = Crawl(main_url)
+    cr_ob.startCrawling()
     
-    #rsp = headPing(main_url)
-    #print repr(rsp)
+    #cr = Crawl()
+    #print repr(Crawl.headPing(main_url))
     
-    #rsp = getContent(main_url)
-    #print repr(rsp.info().headers)
-    #print "\n"
-    #print rsp.read()
-    
-    rsp2 = getContent2(main_url)
-    all = getURLs(rsp2.body)
-    for i in all:
-        print i
-    #print "\n\n".join(all)
+    #v1 = 'binary/html'
+    #v2 = 'binary/application'
+    #print repr(isContentTypeText(v1))
+    #print repr(isContentTypeText(v2))
 
 
 if(__name__ == "__main__"):
