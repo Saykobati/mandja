4b8d8f63d0e2b6937609dc50215d48afebb5c6c3
mr6r4y <mr6r4y@741b1206-e63e-97db-7ddf-8b48874080b5>
2010-12-30 15:51:18 +0000

    Some testing comments were removed.

--- mandja.py
+++ mandja.py
@@ -9,15 +9,6 @@ def main():
     
     cr_ob = Crawl(main_url)
     cr_ob.startCrawling()
-    
-    #cr = Crawl()
-    #print repr(Crawl.headPing(main_url))
-    
-    #v1 = 'binary/html'
-    #v2 = 'binary/application'
-    #print repr(isContentTypeText(v1))
-    #print repr(isContentTypeText(v2))
-
 
 if(__name__ == "__main__"):
     main()
