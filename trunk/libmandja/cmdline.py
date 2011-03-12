9bf2a6fc1e1fbd19bd17270d8966a476f5c5264b
mr6r4y <mr6r4y@741b1206-e63e-97db-7ddf-8b48874080b5>
2011-03-12 19:11:38 +0000

    Move the logic of the current script's argument parser into the script's file.

--- trunk/libmandja/cmdline.py
+++ trunk/libmandja/cmdline.py
@@ -16,26 +16,3 @@ class CmdlineParser(object):
             raise argparse.ArgumentTypeError(cls._netloc_error.format(url = url))
         
         return url
-
-
-class MandjaCmdlineParser(CmdlineParser):
-    _program_description = ('A little recursive python crawler. '
-                            'It is able to list all URLs it has visited. '
-                            'There are options to crawl anonimously via proxy, '
-                            'or to reuse connections when you traversing a specified '
-                            'domain.')
-    _program_epilog = 'Author: mrgray'
-    
-    _url_help = 'The URL you want mandja to start with'
-    
-    _arguments = None
-    
-    def __new__(cls):
-        if(cls._arguments is None):
-            parser = argparse.ArgumentParser(description = cls._program_description, epilog = cls._program_epilog)
-            parser.add_argument("url", type = cls._argparseUrlCheck, help = cls._url_help)
-            cls._arguments = parser.parse_args()
-            
-            return cls._arguments
-        else:
-            return cls._arguments
--- trunk/mandja.py
+++ trunk/mandja.py
@@ -1,7 +1,31 @@
 #!/usr/bin/env python
 
-from libmandja.cmdline import MandjaCmdlineParser
+import argparse
+
+from libmandja.cmdline import CmdlineParser
 from libmandja.crawler import Crawl
+
+class MandjaCmdlineParser(CmdlineParser):
+    _program_description = ('A little recursive python crawler. '
+                            'It is able to list all URLs it has visited. '
+                            'There are options to crawl anonimously via proxy, '
+                            'or to reuse connections when you traversing a specified '
+                            'domain.')
+    _program_epilog = 'Author: mrgray'
+    
+    _url_help = 'The URL you want mandja to start with'
+    
+    _arguments = None
+    
+    def __new__(cls):
+        if(cls._arguments is None):
+            parser = argparse.ArgumentParser(description = cls._program_description, epilog = cls._program_epilog)
+            parser.add_argument("url", type = cls._argparseUrlCheck, help = cls._url_help)
+            cls._arguments = parser.parse_args()
+            
+            return cls._arguments
+        else:
+            return cls._arguments
 
 def main():
     arguments = MandjaCmdlineParser()
